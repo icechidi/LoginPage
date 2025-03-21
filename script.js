ab60ad5d6ad3732d42a68c225e76f727d67ae6a3
Ice Chidi <icechidi3@gmail.com>
2025-03-21 23:59:12 +0200

    JavaScript Initial update, connecting actions to all content

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const formTitle = document.getElementById("form-title");
+    const toggleLogin = document.getElementById("toggle-login");
+    const form = document.getElementById("form");
+    const emailBox = document.getElementById("email-box");
+    const submitButton = document.querySelector(".btn");
+
+    let isLogin = false;
+
+    toggleLogin.addEventListener("click", function(e){
+        e.preventDefault();
+        isLogin = !isLogin;
+
+        if(isLogin){
+            formTitle.textContent = "Login";
+            emailBox.style.display = "none";
+            submitButton.textContent = "Login";
+            toggleLogin.textContent = "Sign Up";
+        }
+    })
+})
